31c6cbcf1ea1c8e2a7f6de3cee74c4c2269bb959
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2020-12-25 13:30:05 +0300

    feat: update readDirectory function

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 const fs = require("fs");
-const fileDir = './'
+const fileDir = "./";
 
 //вывод файлов директории
 //node -e 'require("./index").readDirectory()'
 const readDirectory = () => {
   fs.readdir(`${fileDir}`, (err, files) => {
-    files.forEach((file) => {
-      console.log(file);
-    });
+    if (err) console.log(err);
+    console.log(files);
   });
 };
 
 //чтение файла
 //node -e 'require("./index").readFile("index.js")'
 const readFile = (file) => {
-    fs.readFile(`${fileDir}${file}`, "utf8", (err, data) => {
-        if(err) throw err;
-        console.log(data);
-    })
-}
+  fs.readFile(`${fileDir}${file}`, "utf8", (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+  });
+};
 
-//чтение файла
+//удаление файла
 //node -e 'require("./index").delFile("text.txt")'
 const delFile = (file) => {
-    fs.unlink(`${fileDir}${file}`, (err) => {
-        if (err) console.log(err);   
-        else console.log(`${file} was deleted`);
-      });
-}
+  fs.unlink(`${fileDir}${file}`, (err) => {
+    if (err) console.log(err);
+    else console.log(`${file} was deleted`);
+  });
+};
 
 //создание файла .txt с динамической "шапкой"
 //node -e 'require("./index").createFileTXT("text.txt", "Helloooooooo")'
 const createFileTXT = (file_name, header) => {
   fs.writeFile(`${fileDir}${file_name}`, `${header}`, (err) => {
-    if(err) throw err;
-});
-}
-
+    if (err) console.log(err);
+  });
+};
 
 module.exports = {
-    readDirectory,
-    readFile,
-    delFile,
-    createFileTXT
-}
+  readDirectory,
+  readFile,
+  delFile,
+  createFileTXT,
+};
